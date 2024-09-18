291089fd9383200e04d7dc968eedd80c1ae4cb7b
PequenoMuro <160172087+PequenoMuro@users.noreply.github.com>
2024-09-18 17:12:49 -0300

    Update bd_estabelecimento.sql
    PERDAO PELO COMETIDO

--- MIMUSCLE/bd_estabelecimento.sql
+++ MIMUSCLE/bd_estabelecimento.sql
@@ -5,7 +5,7 @@ use bd_estabelecimento;
 /* Murilo Pierre e Miguel Alves */ 
 
 create table tb_funcionario(
-id_funcionario int primary key,
+id_funcionario int primary key auto_increment,
 nm_funcionario varchar(45),
 sobrenome varchar(45),
 dt_nascimento date,
@@ -16,7 +16,7 @@ senha text
 );
 
 create table tb_cliente(
-id_cliente int primary key,
+id_cliente int primary key auto_increment,
 nm_cliente varchar(45),
 nm_sobrenome varchar(45),
 email varchar(45),
@@ -25,7 +25,7 @@ nm_endereco varchar(45)
 );
 
 create table tb_fornecedor(
-id_fornecedor int primary key,
+id_fornecedor int primary key auto_increment,
 nm_fornecedor varchar(45),
 contato varchar(45),
 email varchar(45),
